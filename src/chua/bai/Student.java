1206305ecfb7e42c028dbcf066d2e5fc78fd3ad9
ThapLV <quangthap9x@gmail.com>
2020-08-21 09:24:42 +0700

    fix bug

---
+++ src/chua/bai/Student.java
@@ -0,0 +1,48 @@
+package chua.bai;
+
+import java.util.Scanner;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void createStudent(){
+        System.out.print("Enter id : ");
+        this.id = Integer.parseInt(new Scanner(System.in).nextLine());
+        System.out.print("Enter name : ");
+        this.name = new Scanner(System.in).nextLine();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
